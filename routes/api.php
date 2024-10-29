972c17531b9b306a553e7f2cef213f067e26d509
AlexRsk <alexrsk@apd.dev>
2024-10-29 19:20:54 +0300

    Поправить ключ для контроллера

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,6 @@ use Ebess\AdvancedNovaMediaLibrary\Http\Controllers\DownloadMediaController;
 use Ebess\AdvancedNovaMediaLibrary\Http\Controllers\MediaController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/download/{media}', [config('nova_media_library.download-controller'), 'show']);
+Route::get('/download/{media}', [config('nova-media-library.download-controller-class'), 'show']);
 
 Route::get('/media', [MediaController::class, 'index']);
