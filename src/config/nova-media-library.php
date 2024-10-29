e1ecd585bcbc51720d7781f63e32b7969c22c142
AlexRsk <alexrsk@apd.dev>
2024-10-29 18:59:45 +0300

    Настраиваемый контроллер скачивания

--- src/config/nova-media-library.php
+++ src/config/nova-media-library.php
@@ -4,4 +4,5 @@ return [
     'default-croppable' => true,
     'enable-existing-media' => false,
     'hide-media-collections' => [],
+    'download-controller-class' => Ebess\AdvancedNovaMediaLibrary\Http\Controllers\DownloadMediaController::class
 ];
